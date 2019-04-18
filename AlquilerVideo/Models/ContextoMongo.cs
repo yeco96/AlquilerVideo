5592e327b465ea2267bfa3f19fb8b7187fa324bd
yeco96 <yeisonpa.96@hotmail.com>
2019-04-18 11:49:21 -0600

    MongoDB

---
+++ AlquilerVideo/Models/ContextoMongo.cs
@@ -0,0 +1,36 @@
+﻿
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fidelitas.NoSql.PrimerEjemplo.Models
+{
+    public class ContextoMongo
+    {
+        public IMongoDatabase database;
+        public ContextoMongo()
+        {
+            var connectionString = AlquilerVideo.Properties.Settings.Default.mongoConnection;
+
+            var client = new MongoClient("mongodb+srv://yeco96:k0nsum0.Respet0@cluster0-6sxzr.azure.mongodb.net/test?retryWrites=true");
+            //var database = client.GetDatabase("test");
+
+
+            database = client.GetDatabase(AlquilerVideo.Properties.Settings.Default.databaseName);
+        }
+
+        //public IMongoCollection<Animales> LosAnimales
+        //{
+        //    get
+        //    {
+        //        return (database.GetCollection<Animales>("animales"));
+        //    }
+        //    set
+        //    {
+        //    }
+        //}
+
+    }
+}
